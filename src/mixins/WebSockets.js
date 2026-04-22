3768b0982ab0f001b01aa6b5281608a4707ae0ed
Lucas <lucasgonzalez5500@gmail.com>
2026-04-22 18:24:56 -0300

    .

--- src/mixins/WebSockets.js
+++ src/mixins/WebSockets.js
@@ -3,39 +3,42 @@ export default {
     mixins: [messages],
 	methods: {
 		listenChannels() {
-            this.Echo.channel('order.'+this.user.id)
-            .notification((notification) => {
-                this.$store.commit('orders/setOrder', notification.order)
-                // this.$store.dispatch('notifications/getUnreadNotifications')
-                if (this.isOrderDelivered(notification)) {
-                    this.$store.dispatch('orders/getCurrentOrder')
-                    this.$store.dispatch('orders/getOrders')
-                }
-            });
-            this.Echo.channel('message.from_commerce.'+this.user.id)
-            .notification((notification) => {
-                this.$store.commit('messages/addMessage', notification.message)
-                this.$store.commit('messages/setMessagesNotRead')
-                console.log(notification)
-                if (this.isOrderDelivered(notification.message)) {
-                    console.log('es order delivered')
-                    this.$store.dispatch('orders/getCurrentOrder')
-                    this.$store.dispatch('orders/getOrders')
-                }
-                if (this.isCartAmountUpdated(notification.message)) {
-                    console.log('es cart amount updated')
-                    this.$store.dispatch('cart/getLastCart')
-                }
-                this.checkIfIsMessagesView()
-            });
-            this.Echo.channel('question.'+this.user.id)
-            .notification((notification) => {
-                this.$store.dispatch('notifications/getUnreadNotifications')
-            });
-            this.Echo.channel('payment.'+this.user.id)
-            .notification((notification) => {
-                this.$store.dispatch('notifications/getUnreadNotifications')
-            });
+            if (this.user && this.user.id) {
+
+                this.Echo.channel('order.'+this.user.id)
+                .notification((notification) => {
+                    this.$store.commit('orders/setOrder', notification.order)
+                    // this.$store.dispatch('notifications/getUnreadNotifications')
+                    if (this.isOrderDelivered(notification)) {
+                        this.$store.dispatch('orders/getCurrentOrder')
+                        this.$store.dispatch('orders/getOrders')
+                    }
+                });
+                this.Echo.channel('message.from_commerce.'+this.user.id)
+                .notification((notification) => {
+                    this.$store.commit('messages/addMessage', notification.message)
+                    this.$store.commit('messages/setMessagesNotRead')
+                    console.log(notification)
+                    if (this.isOrderDelivered(notification.message)) {
+                        console.log('es order delivered')
+                        this.$store.dispatch('orders/getCurrentOrder')
+                        this.$store.dispatch('orders/getOrders')
+                    }
+                    if (this.isCartAmountUpdated(notification.message)) {
+                        console.log('es cart amount updated')
+                        this.$store.dispatch('cart/getLastCart')
+                    }
+                    this.checkIfIsMessagesView()
+                });
+                this.Echo.channel('question.'+this.user.id)
+                .notification((notification) => {
+                    this.$store.dispatch('notifications/getUnreadNotifications')
+                });
+                this.Echo.channel('payment.'+this.user.id)
+                .notification((notification) => {
+                    this.$store.dispatch('notifications/getUnreadNotifications')
+                });
+            }
 		},
         checkIfIsMessagesView() {
             if (this.$route.name == 'Messages') {
